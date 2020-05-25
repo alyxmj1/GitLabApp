62b73b974c8ebe57ef0e61c7cd99d4c86d4c0ff0
alyxmj1 <nasvet2205@gmail.com>
2020-05-25 11:17:43 +0300

    Добавьте файлы проекта.

---
+++ GitLabApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLabApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sx = Console.ReadLine();
+            double x = Convert.ToDouble(sx);
+            double y = (Math.Sin(x) - Math.Cos(x)) / 5;
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
